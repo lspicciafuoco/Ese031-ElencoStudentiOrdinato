c5edb9fb6a63161c97b11cdedbcd7343ca20423e
Alessio.Picciafuoco <alessio.picciafuoco@gmail.com>
2015-04-15 22:30:01 +0200

    commit

--- main.c
+++ main.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 3
+#define MAX_STRLEN 30
 
-// Definisci le costanti
+struct s_studente{
+    char nome[MAX_STRLEN];
+    char cognome[MAX_STRLEN];
+    unsigned int eta;
+    char classe[MAX_STRLEN];
+};
 
-// Definisci il tipo di dato struct s_studente
+typedef struct s_studente studente;
 
-// Definisci studente come struct s_studente
-
-// Dichiara il prototipo della funzione di ordinamento
-
+void Cognomi(studente v[], int n);
 
 int main(int argc, char** argv) {
-    // Dichiara le variabili locali
     
-    // Codice per l'inserimento, da parte dell'utente, dei dati
+    int i;
+    studente elenco[N];
     
-    // Ordina l'elenco (con funzione)
+    for(i=0;i<N;i++){
+        printf("NOME: ",i);
+        scanf("%s", (elenco[i].nome));
+        printf("COGNOME: ",i);
+        scanf("%s", (elenco[i].cognome));
+        printf("ETA: ",i);
+        scanf("%d", &(elenco[i].eta));
+        printf("CLASSE: ",i);
+        scanf("%s", (elenco[i].classe));
+    }
     
-    // Codice per la stampa dei dati (uno studente per riga)
+    Cognomi(elenco,N);
     
+    for(i=0;i<N;i++){
+        printf("%s; %s; %d; %s\n", elenco[i].nome,elenco[i].cognome,elenco[i].eta,elenco[i].classe);
+    }
     return (EXIT_SUCCESS);
 }
 
-// Definisci la funzione di ordinamento
+void Cognomi(studente v[],int n){
+    int i,min,j;
+    studente scam;
+    
+    for(i=0;i<n-1;i++){
+        min=i;
+        
+        for(j=i+1;j<n;j++){
+            if(strcmp(v[j].cognome,v[min].cognome)<0){
+                min=j;
+            }
+        }
+        scam=v[i];
+        v[i]=v[min];
+        v[min]=scam;
+    }
+}
